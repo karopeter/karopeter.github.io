b2dc95a24a7d53a99b06a83e881dd897a27ae565
Karo Peter <Karopeter531@gmail.com>
2020-07-31 08:57:42 -0400

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { HotelComponent } from './hotel/hotel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    HotelComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
